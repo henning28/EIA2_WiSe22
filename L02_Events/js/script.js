f5b3d6ccf7a669d6f60417ef676c6a97f2150d3a
henningreck <reck.henning2@gmail.com>
2022-09-25 20:52:30 +0200

    WIP: L02

---
+++ L02_Events/js/script.js
@@ -0,0 +1,8 @@
+var L02_Load;
+(function (L02_Load) {
+    document.addEventListener("DOMContentLoaded", handleLoad);
+    function handleLoad(_event) {
+        console.log(_event);
+    }
+})(L02_Load || (L02_Load = {}));
+//# sourceMappingURL=script.js.map
