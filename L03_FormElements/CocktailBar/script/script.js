69981c57cd8335a8052ffdac992b2c9a3b2e2b92
henningreck <reck.henning2@gmail.com>
2022-10-03 17:10:42 +0200

    WIP: L03

---
+++ L03_FormElements/CocktailBar/script/script.js
@@ -0,0 +1,26 @@
+var L03_CocktailBar;
+(function (L03_CocktailBar) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log("Start");
+        let form = document.querySelector("div#form");
+        let slider = document.querySelector("input#amount");
+        form.addEventListener("change", handleChange);
+        slider.addEventListener("input", displayAmount);
+    }
+    function handleChange(_event) {
+        // console.log(_event);
+        // let drink: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
+        // console.log(drink.value);
+        // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+        // console.log(inputs);
+        let formData = new FormData(document.forms[0]);
+        console.log(formData.get("Drink"));
+    }
+    function displayAmount(_event) {
+        let progress = document.querySelector("progress");
+        let amount = _event.target.value;
+        progress.value = parseFloat(amount);
+    }
+})(L03_CocktailBar || (L03_CocktailBar = {}));
+//# sourceMappingURL=script.js.map
