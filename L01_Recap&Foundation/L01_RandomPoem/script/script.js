0adb9c3c2f81a3d462427634a152b40b97b16c09
henningreck <reck.henning2@gmail.com>
2022-10-12 11:31:17 +0200

    FIN: RandomPoem

--- L01_Recap&Foundation/L01_RandomPoem/script/script.js
+++ L01_Recap&Foundation/L01_RandomPoem/script/script.js
@@ -2,18 +2,21 @@ var L01_RandomPoem;
 (function (L01_RandomPoem) {
     let characters = ["Po", "Shifu", "Oogway", "Tigress", "Crane", "Mantis"];
     let action = ["isst", "bekämpft", "liebt", "balanciert", "vermeidet", "respektiert"];
-    let objects = ["Dumplings", "den Drachenkrieger", "Nudeln", "Meditation", "den Jadepalast", "die Drachenrolle"];
-    for (let index = 5; index > 0; index--) {
-        getVerse(characters, action, objects);
+    let objects = ["Dumplings", "den Drachenkrieger", "Nudeln", "Pfirsiche", "den Jadepalast", "die Drachenrolle"];
+    for (let iNumber = characters.length; iNumber > 0; iNumber--) {
+        console.log(getVerse(characters, action, objects));
     }
     function getVerse(_characters, _action, _objects) {
         let finishedVerse = "";
-        let randomnumber = Math.floor(Math.random() * _characters.length);
-        let value = _characters.splice(randomnumber, 1);
-        let secondvalue = _action.splice(randomnumber, 1);
-        let thirdvalue = _objects.splice(randomnumber, 1);
-        finishedVerse = value + " " + secondvalue + " " + thirdvalue;
-        console.log(finishedVerse);
+        let randomNumber;
+        randomNumber = Math.floor(Math.random() * _characters.length);
+        let randomCharacter = _characters.splice(randomNumber, 1);
+        randomNumber = Math.floor(Math.random() * _action.length);
+        let randomAction = _action.splice(randomNumber, 1);
+        randomNumber = Math.floor(Math.random() * _objects.length);
+        let randomObject = _objects.splice(randomNumber, 1);
+        finishedVerse = randomCharacter + " " + randomAction + " " + randomObject;
+        return finishedVerse;
     }
 })(L01_RandomPoem || (L01_RandomPoem = {}));
 //# sourceMappingURL=script.js.map
--- L01_Recap&Foundation/L01_RandomPoem/script/script.ts
+++ L01_Recap&Foundation/L01_RandomPoem/script/script.ts
@@ -3,27 +3,27 @@ namespace L01_RandomPoem {
 
     let action: string[] = ["isst", "bekämpft", "liebt", "balanciert", "vermeidet", "respektiert"];
 
-    let objects: string[] = ["Dumplings", "den Drachenkrieger", "Nudeln", "Meditation", "den Jadepalast", "die Drachenrolle"];
+    let objects: string[] = ["Dumplings", "den Drachenkrieger", "Nudeln", "Pfirsiche", "den Jadepalast", "die Drachenrolle"];
 
-
-    for (let index: number = 5; index > 0; index--) {
-        getVerse(characters, action, objects);
+    for (let iNumber: number = characters.length; iNumber > 0; iNumber--) {
+        console.log(getVerse(characters, action, objects));
     }
 
-    function getVerse(_characters: string[], _action: string[], _objects: string[]): void {
+    function getVerse(_characters: string[], _action: string[], _objects: string[]): string {
         let finishedVerse: string = "";
+        let randomNumber: number;
 
-        let randomnumber: number = Math.floor(Math.random() * _characters.length);
+        randomNumber = Math.floor(Math.random() * _characters.length);
+        let randomCharacter: string[] = _characters.splice(randomNumber, 1);
 
-        let value: string[] = _characters.splice(randomnumber, 1);
+        randomNumber = Math.floor(Math.random() * _action.length);
+        let randomAction: string[] = _action.splice(randomNumber, 1);
 
-        let secondvalue: string[] = _action.splice(randomnumber, 1);
+        randomNumber = Math.floor(Math.random() * _objects.length);
+        let randomObject: string[] = _objects.splice(randomNumber, 1);
 
-        let thirdvalue: string[] = _objects.splice(randomnumber, 1);
+        finishedVerse = randomCharacter + " " + randomAction + " " + randomObject;
 
-        finishedVerse = value + " " + secondvalue + " " + thirdvalue;
-
-        console.log(finishedVerse);
-
+        return finishedVerse;
     }
 }
