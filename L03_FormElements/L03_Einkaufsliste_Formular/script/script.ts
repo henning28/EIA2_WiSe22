62350ba8360cfdad93140963e430e2968f919098
henningreck <reck.henning2@gmail.com>
2022-10-28 10:52:25 +0200

    ADD: L03

--- L03_FormElements/L03_Einkaufsliste_Formular/script/script.ts
+++ L03_FormElements/L03_Einkaufsliste_Formular/script/script.ts
@@ -1,3 +1,39 @@
+/*
+Aufgabe: L02_EventInspector
+Name: Henning Reck
+Matrikel: 271133
+Datum: 28.10.2022
+Quellen:
+*/
+
 namespace L03_Einkaufsliste_Formular {
-    console.log("yup");
+    window.addEventListener("load", handleLoad);
+
+    let addItemButton: HTMLButtonElement = document.querySelector("button");
+
+    function handleLoad(): void {
+        addItemButton.addEventListener("click", buttonClick);
+
+        document.querySelector(".trashcan").addEventListener("click", deleteItem);
+        
+        document.querySelector(".editbutton").addEventListener("click", editItem);
+
+        document.querySelector(".checkbox").addEventListener("click", checkItem);
+    }
+
+    function buttonClick(): void {
+        console.log("button pressed, create new Item (Name, Amount, Comment, Date)");
+    }
+
+    function deleteItem(): void {
+        console.log("trash pressed, delete Item");
+    }
+
+    function editItem(): void {
+        console.log("edit button pressed, edit Item");
+    }
+
+    function checkItem(): void {
+        console.log("checkbox pressed");
+    }
 }
